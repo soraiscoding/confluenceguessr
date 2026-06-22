bdbd0510efe01ccf46d9d4c20ae74dabf3f40ca4
rich-z00 <richardzhou8@hotmail.com>
2026-06-22 23:34:44 +1000

    added a question screen so I can test to make sure create game takes you to a different screen

--- static/src/App.js
+++ static/src/App.js
@@ -1,6 +1,9 @@
 //import React, { useEffect, useState } from 'react'; probs don't need this since it'll be in the components' files
 //import { invoke } from '@forge/bridge'; this is to call the backend in index.js in ../../src
+
+// import screens
 import CreateGame from './screens/CreateGame.js';
+import QuestionScreen from './screens/QuestionScreen.js';
 
 function App() {
   //const [data, setData] = useState(null);
